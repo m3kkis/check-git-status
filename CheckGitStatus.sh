8e3b735300d8a6eeb5ff3c97020fd93e2419312d
Daniel Mekker <daniel.mekker@gmail.com>
2020-03-17 16:07:32 -0400

    added branch name

--- CheckGitStatus.sh
+++ CheckGitStatus.sh
@@ -3,19 +3,21 @@
 #check if its a git directory then check status 
 #and at the end of execution, press any key to exit
 
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+NOCOLOR="\033[0m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+PURPLE="\033[0;35m"
 
 for d in */ ; do
 	cd $d
 	if [ -d .git ]; then 
 		if [[ `git status --porcelain` ]]; then
-			echo -e "${RED}"$d"${NOCOLOR}"
+			echo -e "${PURPLE}"[`git branch --show-current`] "${RED}"$d"${NOCOLOR}"
 		else
-			echo -e "${GREEN}"$d"${NOCOLOR}"
+			echo -e "${PURPLE}"[`git branch --show-current`] "${GREEN}"$d"${NOCOLOR}"
 		fi
 	fi
 	cd ..
 done
-read -n 1 -s -r -p "Press any key to continue"
+printf "\n"
+read -n 1 -s -r -p "Press any key to continue..."
